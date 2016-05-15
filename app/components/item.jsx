ac5b7325d6bb2b75a54d06e2148140023998e7c6
yongmin.cai <yongmin.cai@fraudmetrix.cn>
2016-05-15 11:52:47 +0800

    完成删除功能

--- app/components/item.jsx
+++ app/components/item.jsx
@@ -4,6 +4,19 @@ import {Link} from 'react-router'
 export default class Item extends React.Component {
   	constructor(props) {
     	super(props)
+    	this.onDeleteItem = this.onDeleteItem.bind(this)
+  	}
+
+  	onDeleteItem() {
+  		let taskId = this.refs.taskId.value.trim()
+
+  		if (!taskId) {
+  			alert('this is no task id')
+  			return
+  		}
+  		
+  		this.props.setDeleteTaskId(taskId)
+  		this.props.showPop()
   	}
 
   	render() {
@@ -12,14 +25,15 @@ export default class Item extends React.Component {
 		return (
 			<li className="task-item">
 				<p className="task-item-hd">
-					<span className="task-item-title">{task.name}</span>
+					<span className="task-item-title"><Link className="task-item-link" to={`task/${task.id}`}>{task.name}</Link></span>
 					<span className="task-item-date">{task.created}</span>
+					<input type="hidden" ref="taskId" value={task.id} />
 				</p>
-				<p className="task-item-body">
+				<p className="task-item-bd">
 					<i className={'fa ' + (task.finished ? 'fa-check-square-o state-finished' : 'fa-clock-o state-unfinished')}></i>
 					<span className="task-item-status">{task.finished ? '已完成' : '未完成'}</span>
+					<i className="fa fa-remove task-item-delete" onClick={this.onDeleteItem}></i>
 				</p>
-				<Link to={`task/${task.id}`}></Link>
 			</li>
 		)
   	}
--- app/handler/tasks.jsx
+++ app/handler/tasks.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
 
 import Item from '../components/item'
+import DeleteItem from '../components/deleteitem'
 
 export default class Tasks extends React.Component {
   	constructor(props) {
     	super(props)
+    	this.state = {showDelPop: false, willDeleteTaskId: -1}
     	this.showPop = this.showPop.bind(this)
+    	this.showDeletePop = this.showDeletePop.bind(this)
+    	this.hideDeletePop = this.hideDeletePop.bind(this)
+    	this.setDeleteTaskId = this.setDeleteTaskId.bind(this)
+    	this.deleteItem = this.deleteItem.bind(this)
   	}
 
   	showPop() {
   		this.props.showPop()
+  	}
+
+  	showDeletePop() {
+  		this.setState({showDelPop: true})
+  	}
+
+  	hideDeletePop() {
+  		this.setState({showDelPop: false})
+  	}
+
+  	setDeleteTaskId(id) {
+  		this.setState({willDeleteTaskId: id})
+  	}
+
+  	deleteItem() {
+  		let taskId = this.state.willDeleteTaskId
+  		this.props.deleteTask(taskId)
+  		this.hideDeletePop()
   	}
 
   	render() {
@@ -32,13 +56,23 @@ export default class Tasks extends React.Component {
 
 		if (Array.isArray(tasks) && tasks.length) {
 			let tasksDOM = tasks.map(function (task, index) {
-				return (<Item key={task.id} task={task}/>)
-			})
+				return (
+					<Item
+						showPop={this.showDeletePop}
+						key={task.id}
+						task={task}
+						setDeleteTaskId={this.setDeleteTaskId}/>
+				)
+			}.bind(this))
 			return (
 				<div className="task-list-wrap">
 					<ul className="task-list">
 						{tasksDOM}
 					</ul>
+					<DeleteItem 
+						showDelPop={this.state.showDelPop}
+						hideDeletePop={this.hideDeletePop}
+						deleteItem={this.deleteItem}></DeleteItem>
 				</div>
 			)
 		} else {
@@ -46,21 +80,27 @@ export default class Tasks extends React.Component {
 				case 0:
 					return (
 						<div className="task-list-wrap">
-							<p className="tip"><span>暂时没有未完成任务. 你可以点击 </span><i className="fa fa-plus" onClick={this.showPop}></i><span> 添加任务。</span></p>
+							<p className="tip">
+								<span>暂时没有未完成任务. 你可以点击 </span><i className="fa fa-plus" onClick={this.showPop}></i><span> 添加任务。</span>
+							</p>
 						</div>
 					)
 					break
 				case 1:
 					return (
 						<div className="task-list-wrap">
-							<p className="tip"><span>暂时没有已完成任务. 你可以点击 </span><i className="fa fa-plus" onClick={this.showPop}></i><span> 添加任务，然后完成它。</span></p>
+							<p className="tip">
+								<span>暂时没有已完成任务. 你可以点击 </span><i className="fa fa-plus" onClick={this.showPop}></i><span> 添加任务，然后完成它。</span>
+							</p>
 						</div>
 					)
 					break
 				default:
 					return (
 						<div className="task-list-wrap">
-							<p className="tip"><span>暂时没有任务, 你可以点击 </span><i className="fa fa-plus" onClick={this.showPop}></i><span> 添加任务。</span></p>
+							<p className="tip">
+								<span>暂时没有任务, 你可以点击 </span><i className="fa fa-plus" onClick={this.showPop}></i><span> 添加任务。</span>
+							</p>
 						</div>
 					)
 					break
